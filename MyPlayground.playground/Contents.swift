8b8951d9a1163e754552f723536af80ff3cf01c3
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-10-15 14:21:39 -0400

    added one more

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -488,3 +488,60 @@ func numberOfPoints(_ nums: [[Int]]) -> Int {
     return Set(holder).count
   }
 numberOfPoints([[1,3],[5,8]])
+
+
+//
+//You are given two positive integers low and high.
+//An integer x consisting of 2 * n digits is symmetric if the sum of the first n digits of x is equal to the sum of the last n digits of x. Numbers with an odd number of digits are never symmetric.
+//Return the number of symmetric integers in the range [low, high].
+//Example 1:
+//Input: low = 1, high = 100
+//Output: 9
+//Explanation: There are 9 symmetric integers between 1 and 100: 11, 22, 33, 44, 55, 66, 77, 88, and 99.
+//Example 2:
+//Input: low = 1200, high = 1230
+//Output: 4
+//Explanation: There are 4 symmetric integers between 1200 and 1230: 1203, 1212, 1221, and 1230.
+//
+func sumTwoStrInt(_ strNum:String)->Int{
+    var sum = 0
+    for i in strNum {
+//        print(i)
+        sum+=Int(String(i)) ?? 0
+    }
+    return sum
+}
+//sumTwoStrInt("33")   // 6
+func isSymetric(_ num:Int)->Bool{
+    var numAsStr = String(num)
+    var leftSlice:String = ""
+    var rightSlice:String = ""
+    if numAsStr.count % 2 == 0 {
+        for (i,char) in numAsStr.enumerated(){
+            if i < numAsStr.count/2 {
+                leftSlice.append(char)
+            }else {
+                rightSlice.append(char)
+            }
+        }
+        if sumTwoStrInt(leftSlice) == sumTwoStrInt(rightSlice){
+            return true
+        }
+    
+    }
+  
+//   print(rightSlice , leftSlice)
+    return false
+}
+//isSymetric(3324)
+
+func countSymmetricIntegers(_ low: Int, _ high: Int) -> Int {
+    var count = 0
+    for i in low...high{
+        if isSymetric(i){
+            count+=1
+        }
+    }
+    return count
+ }
+print(countSymmetricIntegers(1200,1230))
